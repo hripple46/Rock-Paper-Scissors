8fbdb4b3ca14f827077fc87a716486179b5a9a76
Henry Ripple <hripple46@gmail.com>
2022-11-02 15:51:02 -0500

    reconfigured program as a function playRound with two parameters (playerchoice and computer choice)

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,39 +1,39 @@
 console.log("Hi");
-
 const computerMove = ["Rock", "Paper", "Scissors"];
 
-let userPrompt = prompt("Rock, Paper, or Scissors?");
-
-//checking if user input is valid entry
-if (userPrompt.toUpperCase() == "ROCK") {
-  computerMove[Math.floor(Math.random() * computerMove.length)];
-  if (
-    computerMove[Math.floor(Math.random() * computerMove.length)] == "Paper"
-  ) {
-    alert("Paper! \nYou Lose!");
-  } else {
-    alert("Computer Chooses Paper! \nYou Win");
-  }
-} else if (userPrompt.toUpperCase() == "SCISSORS") {
-  computerMove[Math.floor(Math.random() * computerMove.length)];
-  if (computerMove[Math.floor(Math.random() * computerMove.length)] == "Rock") {
-    alert("Rock! \nYou Lose!");
-  } else {
-    alert("Computer Chooses Rock! \nYou Win!");
-  }
-} else if (userPrompt.toUpperCase() == "PAPER") {
-  computerMove[Math.floor(Math.random() * computerMove.length)];
-  if (
-    computerMove[Math.floor(Math.random() * computerMove.length)] == "Scissors"
-  ) {
-    alert("Scissors! \nYou Lose!");
-  } else {
-    alert("Computer Chooses Rock! \nYou Win!");
-  }
-} else {
-  prompt("Try Again!");
-}
+const userPrompt = prompt("Rock, Paper, or Scissors?");
 
 function getComputerChoice() {
-  computerMove[Math.floor(Math.random() * computerMove.length)];
+  return computerMove[Math.floor(Math.random() * computerMove.length)];
 }
+let computerChoice = getComputerChoice();
+
+//checking if user input is valid entry
+
+function playRound(playerChoice, computerChoice) {
+  if (userPrompt.toUpperCase() == "ROCK") {
+    computerChoice;
+    if (computerChoice == "Paper") {
+      alert("Paper! \nYou Lose!");
+    } else {
+      alert("Computer Chooses Paper! \nYou Win");
+    }
+  } else if (playerChoice.toUpperCase() == "SCISSORS") {
+    computerChoice;
+    if (computerChoice == "Rock") {
+      alert("Rock! \nYou Lose!");
+    } else {
+      alert("Computer Chooses Rock! \nYou Win!");
+    }
+  } else if (playerChoice.toUpperCase() == "PAPER") {
+    computerChoice;
+    if (computerChoice == "Scissors") {
+      alert("Scissors! \nYou Lose!");
+    } else {
+      alert("Computer Chooses Rock! \nYou Win!");
+    }
+  } else {
+    prompt("Try Again!");
+  }
+}
+playRound(userPrompt, computerChoice);
